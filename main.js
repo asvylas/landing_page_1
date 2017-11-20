f13f977ce8331f73a6921ab0f657f1c4f79f75b4
Andrius Svylas <32981972+asvylas@users.noreply.github.com>
2017-11-20 15:46:45 +0200

    Landing Page Finished
    Fisnihed Landing Page 1

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-(function() {
+const  dataText = document.querySelectorAll("div[data-text]")
+const iconContainer = document.getElementById("icon-container");
+const contentContainer = document.getElementById("content-container");
 
-	const  dataText = document.querySelectorAll("p[data-text]")
-	const iconContainer = document.getElementById("icon-container");
-	const contentContainer = document.getElementById("content-container");
+const navIcons = document.querySelectorAll("div.flexitem")
 
-	const navIcons = document.querySelectorAll("div.flexitem")
+ for(var i=0; i< navIcons.length; i++) {
+	   navIcons[i].addEventListener("click", bindClick(i));
+ }
 
-	 for(var i=0; i< navIcons.length; i++) {
-		   navIcons[i].addEventListener("click", bindClick(i));
-	 }
-
-	 function bindClick(i) {
-		return function(){
-				 for (let a = 0; a < dataText.length ; a++){
-					dataText[a].className = "hidden"
-				 };
-				 dataText[i].className = "show"
-			   };
-	 }
-	 
-})();
+ function bindClick(i) {
+	return function(){
+	
+	
+			 for (let a = 0; a < dataText.length ; a++){
+				dataText[a].className = "hidden"
+				navIcons[a].className="flexitem"
+			 };
+			 
+			 dataText[i].className = "show"
+			 navIcons[i].className="flexitemclicked"
+		   };
+ }
